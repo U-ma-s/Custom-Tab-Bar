ca2d13e87360a3d1960962a7650430b47b782d95
U-ma-s <yuma.prodia4989@gmail.com>
2023-05-19 23:59:42 +0900

    Initial Commit

---
+++ Custom Tab Bar/Custom_Tab_BarApp.swift
@@ -0,0 +1,18 @@
+//
+//  Custom_Tab_BarApp.swift
+//  Custom Tab Bar
+//  
+//  Created by umas on 2023/05/19
+//  
+//
+
+import SwiftUI
+
+@main
+struct Custom_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
